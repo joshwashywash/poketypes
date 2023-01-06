8c3d7a597d26b758d20c50fcb200b28637b2be1d
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-01-05 19:26:28 -0800

    add d3 forces

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,7 @@
 
 	type Node = SimulationNodeDatum & { id: string };
 
-	const nodes: Node[] = poketypes.map(({ name }, i) => {
+	let nodes: Node[] = poketypes.map(({ name }, i) => {
 		const theta = i * angle;
 		const [x, y] = [Math.cos, Math.sin].map(f => f(theta));
 		return { id: name, x, y };
@@ -37,22 +37,29 @@
 	const diameter = 2 * radius;
 	const viewBox = [-radius, diameter].flatMap(i => [i, i]).join(' ');
 
-	let nodies: Node[] = [];
 	type ForcedLink = { source: Node; target: Node; id: string };
 	let linkies: ForcedLink[] = [];
 
-	forceSimulation<Node, Link>(nodes)
+	const sim = forceSimulation<Node, Link>(nodes)
 		.force('center', forceCenter())
-		.force('many', forceManyBody())
+		.force('collide', forceManyBody())
 		.force(
 			'links',
 			forceLink<Node, Link>(links).id(d => d.id)
 		)
 		.on('tick', () => {
-			nodies = nodes;
+			nodes = nodes;
 			// d3 mutates the links by converting the source and target from a string to a Node. it also adds the id prop
 			linkies = links as ForcedLink[];
 		});
+
+	const createArcPath = (link: ForcedLink) => {
+		const { source, target } = link;
+		const r = Math.hypot(source.x - target.x, source.y - target.y);
+		return `M${source.x},${source.y} A${r},${r} 0 0,1 ${target.x},${target.y}`;
+	};
+
+	let heldNode: Node = null;
 </script>
 
 <svelte:head>
@@ -60,7 +67,24 @@
 </svelte:head>
 
 <main class="flex max-h-screen flex-col items-center">
+<h1>drag around the points</h1>
 	<svg
+		on:pointermove={e => {
+			if (heldNode) {
+				const { x, y, currentTarget } = e;
+				const point = new DOMPoint(x, y).matrixTransform(
+					currentTarget.getScreenCTM().inverse()
+				);
+				heldNode.fx = point.x;
+				heldNode.fy = point.y;
+			}
+		}}
+		on:pointerup={() => {
+			if (heldNode) {
+				heldNode = null;
+				sim.alphaTarget(0);
+			}
+		}}
 		class="w-full"
 		fill="none"
 		stroke-linecap="round"
@@ -68,18 +92,27 @@
 		xmlns="http://www.w3.org/2000/svg"
 	>
 		<g stroke-width={strokeWidth}>
-			{#each linkies as { source, target }}
-				<line
-					stroke={colors.get(source.id) ?? 'transparent'}
-					x1={source.x}
-					y1={source.y}
-					x2={target.x}
-					y2={target.y}
+			{#each linkies as link}
+				<path
+					stroke-opacity={link.source.id === heldNode?.id ? 1 : 0.7}
+					stroke={colors.get(link.source.id) ?? 'transparent'}
+					d={createArcPath(link)}
 				/>
 			{/each}
 		</g>
-		{#each nodies as { x, y, id }}
-			<circle fill={colors.get(id) ?? 'transparent'} cx={x} cy={y} {r} />
+		{#each nodes as node}
+			<circle
+				class:cursor-grabbing={node.id === heldNode?.id}
+				class:cursor-grab={heldNode === null}
+				on:pointerdown={() => {
+					heldNode = node;
+					sim.alphaTarget(0.3).restart();
+				}}
+				fill={colors.get(node.id) ?? 'transparent'}
+				cx={node.x}
+				cy={node.y}
+				{r}
+			/>
 		{/each}
 	</svg>
 </main>
